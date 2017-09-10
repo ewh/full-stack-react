f6b74514233f6106a24b1a1c173dc29f07efadc0
Weston Hunter <weston.hunter@gmail.com>
2017-09-09 22:12:54 -0500

    Updates.

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send({hi: 'there'});
 });
 
+app.get('/other', (req, res) => {
+    res.send(process.env);
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
